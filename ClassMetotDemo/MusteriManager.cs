ffbbc73ddfe62d1bb40722a15b588c6f6a573959
Enes Uludağ <71eneseu@gmail.com>
2023-11-02 17:04:34 +0300

    16 Ocak Ödev 3

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi : " + musteri.Adi + " " + musteri.Soyadi);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi : " + musteri.Adi + " " + musteri.Soyadi);
+        }
+
+        public void Güncelle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri güncellendi : " + musteri.Adi + " " + musteri.Soyadi);
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            
+            Musteri musteri1 = new Musteri()
+            {
+                Id = 1,
+                Adi = "Enes",
+                Soyadi = "Uludağ",
+                Memleketi = "Kırıkkale"
+            };
+
+            Musteri musteri2 = new Musteri()
+            {
+                Id = 2,
+                Adi = "Melih",
+                Soyadi = "Özdemir",
+                Memleketi = "Malatya"
+            };
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            Console.WriteLine("----------Musteriler----------");
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adi + " " + musteri.Soyadi);
+            }
+
+            Console.WriteLine("------------------------------");
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Sil(musteri1);
+            musteriManager.Sil(musteri2);
+
+            musteri1.Adi = "Emre";
+            musteri2.Soyadi = "Özdamar";
+            musteriManager.Güncelle(musteri1);
+            musteriManager.Güncelle(musteri2);
         }
     }
 }
